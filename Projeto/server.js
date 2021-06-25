ea73bddca886f76a63a499f88fea97a330dc4df1
Ronaldo Teixeira <ronaldotj2002@yahoo.com.br>
2021-06-25 01:42:39 -0300

    Ajustes para deploy

---
+++ Projeto/server.js
@@ -0,0 +1,12 @@
+  
+
+const express = require('express');
+const app = express();
+const appName = 'angular-material-schematics';
+const outputPath = `${__dirname}/dist/${appName}`;
+
+app.use(express.static(outputPath));
+app.get('/*', (req, res) => {
+  res.sendFile(`${outputPath}/index.html`);
+});
+app.listen(process.env.PORT);
